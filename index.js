5264f14640d3f185e305606fa81de50e07421cc3
Ashtonn77 <ashtonrbay@gmail.com>
2020-07-30 18:30:37 +0200

    added local storage functionality

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
-const dummy_transactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
-];
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
 
-let transactions = dummy_transactions;
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
 //add new transaction
 function addTransaction(e) {
@@ -31,6 +29,8 @@ function addTransaction(e) {
 
     addTransactionDOM(transaction);
     updateValues();
+
+    updateLocalStorage();
 
     text.value = "";
     amount.value = "";
@@ -93,7 +93,14 @@ function updateValues() {
 function removeTransaction(id) {
   transactions = transactions.filter((transaction) => transaction.id !== id);
 
+  updateLocalStorage();
+
   init();
+}
+
+//update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 }
 
 function init() {
